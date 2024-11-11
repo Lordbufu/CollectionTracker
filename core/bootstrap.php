d08645eebe8475e43c11ef8fa7edc1e1bf0af735
Lordbufu <bufu_39@hotmail.com>
2024-11-11 15:13:14 +0100

    Correcting a loop issues in the header, and removing some old use directives.

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,9 +1,11 @@
 <?php
+
     /*  bootstrap.php:
             Here i mainly bootstrap App bindings, that allow mw to more easily use certain classes.
             It basically left over from the online course this App is based on, i see no reason to move/change this atm.
      */
-    use App\Core\{ App, Processing, SessionMan, User, Collection, Isbn };
+
+    use App\Core\{ App, SessionMan, User, Collection, Isbn };
     use App\Core\Database\{ QueryBuilder, Connection };
 
     App::bind( "config", require "../config.php" );                                                             // Create a binding for the DB config file.
@@ -12,4 +14,5 @@
     App::bind( "collection", new Collection );                                                                  // Create a binding for the Collection class
     App::bind( "session", new SessionMan );                                                                     // Create a binding for the Session Manager class.
     App::bind( "isbn", new Isbn );                                                                              // Create a binding for the ISBN class.
+    
 ?>
