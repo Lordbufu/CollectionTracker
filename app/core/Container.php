155acf75eca544faff8bd7bc7ca0a1d80ef92673
LordBufu <bufu_39@hotmail.com>
2025-03-03 15:56:55 +0100

    Cleaned up some of the Container code/comments.

--- app/core/Container.php
+++ app/core/Container.php
@@ -2,24 +2,26 @@
 
 namespace App\Core;
 
-use Exception;      // This could be ommited, but i like having it here for clarity reasons.
-
-/*  Container Class:
-        This class is used to create a App Service Container, allowing a easy interface with the App.
-        Making the code more readable, and easier to use (in theory), similar to a API approach.
- */
 class Container {
     protected $bindings = [];
 
-    /*  bind($name, $resolver): A function to bind a name, to a class object (resolver) that i would like to use. */
+    /*  bind($name, $resolver):
+            A function to bind a name, to a class object (resolver) that i would like to use.
+
+            Return Value: None.
+     */
     public function bind($name, $resolver) {
         $this->bindings[$name] = $resolver;
     }
 
-    /*  resolve($key): Attempts to match a string value, with a bound class object, return the object as closure (useable class object) */
+    /*  resolve($key):
+            Attempts to match a string value, with a bound class object.
+
+            Return Value: Class Object.
+     */
     public function resolve($key) {
         if( !array_key_exists($key, $this->bindings) ) {
-            throw new Exception("No matching binding for {$key}");
+            throw new \Exception("No matching binding for {$key}");
         }
 
         $resolver = $this->bindings[$key];
