a4f36ff570bcbaf063547b9efca3fb9a4e673e25
LordBufu <bufu_39@hotmail.com>
2025-03-04 11:50:24 +0100

    Cleaned up the FileHandler comments.

--- app/core/FileHandler.php
+++ app/core/FileHandler.php
@@ -9,81 +9,93 @@ class FileHandler {
     protected $tempFile;
     protected $convFile;
 
-    /*  setFile($file): */
+    /*  setFile($file):
+            With this function i simply set the file variable, to the file being requested for processing (always images ¿).
+                $file (file)    - The file being requested to processing.
+
+            Return Value: Boolean.
+     */
     protected function setFile($file) {
-        /* Set global file with parameter file, */
         $this->file = $file;
 
-        /* return FALSE if no array was set, */
         if(!is_array($this->file)) {
             return FALSE;
         }
 
-        /* return true if all seems well. */
         return TRUE;
     }
 
-    /*  extractData(): */
+    /*  extractData():
+            Here i attempt to extract he usefull data from a image file, so i can convert it to be stored in the database.
+
+            Return Value: Boolean.
+     */
     protected function extractData() {
-        /* Try to set all relevant info to the tempFile variable. */
         $this->tempFile['name'] = basename($this->file['name']);
         $this->tempFile['type'] = pathinfo($this->tempFile['name'], PATHINFO_EXTENSION);
         $this->tempFile['content'] = file_get_contents($this->file['tmp_name']);
 
-        /* Return if the the tempFile is now an array. */
         return is_array($this->tempFile);
     }
 
-    /*  setString() */
+    /*  setString():
+            This function converts the image data, to a useable base64 string, that can be stored and displayed.
+                $dataPart (String)      - The data part of the string, saying its and image and what type of image.
+                $charsetPart (String)   - The charset part, where the base64 converted data gets concatinated.
+
+                Return Value: String.
+     */
     protected function setString() {
-        /* Prepare the string in 2 parts, appending information from the tempFile variable. */
         $dataPart = 'data:image/' . $this->tempFile['type'];
         $charsetPart = ';charset=utf8;base64,' . base64_encode($this->tempFile['content']);
 
-        /* Combine both halfs into a full base64 string. */
         $this->convFile = $dataPart . $charsetPart;
 
-        /* Return if a string was made. */
         return is_string($this->convFile);
     }
 
-    /*  procFile($inpFile): */
+    /*  procFile($inpFile):
+            This function is the controller side of the file processing, and is used to trigger the protected functions above.
+                $inpFile (File/Array)   - The file as presented by the HTML input.
+
+            Return Value:
+                On success: base64 string.
+                On failure: error string.
+     */
     public function procFile($inpFile) {
-        /* If the provided data is not a array, and has a error code, */
         if(!is_array($inpFile)) {
-            /* Return a costum feedback error for the user. */
             return App::resolve('errors')->getError('fileHand', 'no-file');
         }
 
-        /* Attempt to set the file to the global store. */
         if($this->setFile($inpFile)) {
-            /* If the data cant be extracted, return a costum feedback error for the user. */
             if(!$this->extractData()) {
                 return App::resolve('errors')->getError('fileHand', 'proc-fail');
             }
         }
 
-        /* If a string is set, return the string or a feedback error. */
         if($this->setString()) {
             return (is_string($this->convFile)) ? $this->convFile : App::resolve('errors')->getError('fileHand', 'no-string');
         }
     }
 
-    /*  procUrl($url): */
+    /*  procUrl($url):
+            A function to process images from a url link, converting it to a base64 string, to store and display.
+                $url (String)           - Url string as provided by the Google API.
+                $ch (Object)            - A curl stream to read the url data.
+                $data (Object/Array)    - The image data as extracted from the Google API link.
+            
+            Return Value: base64 String.
+     */
     public function procUrl($url) {
-        /* Init curl to request the image data from the Google api url. */
         $ch = curl_init();
 
-        /* Some curl stuff i copt and pasted, not 100% sure what it does. */
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_URL, $url);
 
-        /* Save the data curl extracted, and close curl to finalize it. */
         $data = curl_exec($ch);
         curl_close($ch);
 
-        /* Return the base64 converted string, that can be stored in the database, and show in a html <img> tag. */
         return 'data:image/jpeg;charset=utf8;base64,' . base64_encode($data);
     }
 }
