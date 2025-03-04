867f05c9febf860724a90cd535e666b32363449e
LordBufu <bufu_39@hotmail.com>
2025-03-04 15:09:39 +0100

    Cleaned up the collection add controller.

--- app/http/controllers/collectie/add.php
+++ app/http/controllers/collectie/add.php
@@ -2,10 +2,9 @@
 
 use App\Core\App;
 
-$store = 'empty';                                                                                   // Set useless string for evaluation reasons.
-
-if(isset($_POST['index']) && isset($_SESSION['page-data']['huidige-reeks'])) {                      // Check if we have the required data,
-    $collData = [                                                                                   // prep it as an array for the collection add request.
+/* If the expected inputs are set, create a collection data array. */
+if(isset($_POST['index']) && isset($_SESSION['page-data']['huidige-reeks'])) {
+    $collData = [
         'iIndex' => $_POST['index'],
         'rIndex' => App::resolve('reeks')->getId([
             'Reeks_Naam' => $_SESSION['page-data']['huidige-reeks']
@@ -13,30 +12,44 @@ if(isset($_POST['index']) && isset($_SESSION['page-data']['huidige-reeks'])) {  
     ];
 }
 
-if(isset($collData['iIndex']) && isset($collData['rIndex'])) {                                      // Check if required data was set,
-    $store = App::resolve('collectie')->addColl($collData);                                         // and add item to user collecton.
+/* If the array data wasnt set, or if the reek index resolved to a error, store the correct input and redirect back to the user page. */
+if(!isset($collData) || is_string($collData['rIndex'])) {
+    if(!isset($collData)) {
+        App::resolve('session')->flash('feedback', [
+            'error' => App::resolve('errors')->getError('forms', 'input-missing')
+        ]);
+    } else {
+        App::resolve('session')->flash('feedback', [
+            'error' => $collData['rIndex']
+        ]);
+    }
+
+    return App::redirect('gebruik', TRUE);
 }
 
-if(is_string($store)) {                                                                             // If issues or errors,
-    $mssg = isset($store) ? $store : App::resolve('errors')->getError('forms', 'input-missing');    // check what error to store,
+/* Attemp to add the item to the user collection. */
+$store = App::resolve('collectie')->addColl($collData);
 
-    App::resolve('session')->flash('feedback', [                                                    // prep error as user feedback,
+/* Store user feedback on error, and redirect back to the user page. */
+if(is_string($store)) {
+    App::resolve('session')->flash('feedback', [
         'error' => $store
     ]);
 
-    return App::redirect('gebruik', TRUE);                                                          // and redirect to gebruik-page with _flash tags.
+    return App::redirect('gebruik', TRUE);
 }
 
-$iName = App::resolve('items')->getName([                                                           // Get item name,
+/* Get the item name of the item that was added, prepare the correct user feedback and refresh the collection data before redirecting back to the user page */
+$iName = App::resolve('items')->getName([
     'Item_Index' => $_POST['index']
 ]);
 
-App::resolve('session')->flash('feedback', [                                                        // Tell user {item-name} was added,
+App::resolve('session')->flash('feedback', [
     'added' => "Het item: {$iName}. \n  Is aan uw collectie toegevoegd."
 ]);
 
-App::resolve('session')->setVariable('page-data', [                                                 // Update user there collection data,
+App::resolve('session')->setVariable('page-data', [
     'collecties' => App::resolve('collectie')->getColl()
 ]);
 
-return App::redirect('gebruik', TRUE);                                                              // Redirect to gebruik-page with _flash tags.
+return App::redirect('gebruik', TRUE);
