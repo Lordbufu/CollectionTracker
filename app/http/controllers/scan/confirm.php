192926c6f0343e8c19a4b5f8cdf2c7a14f4fb8d6
LordBufu <bufu_39@hotmail.com>
2025-03-10 11:46:45 +0100

    Cleaned up the isbn confirm title code a bit, more work left to be done.

--- app/http/controllers/scan/confirm.php
+++ app/http/controllers/scan/confirm.php
@@ -5,11 +5,7 @@ use App\Core\App;
 /* Attempt to get the user choice item, via my isbn class. */
 $store = App::resolve('isbn')->confirmChoice($_POST);
 
-/*  If a string is returned,
-    i store the feedback in the flash memory,
-    and return to the default beheer page,
-    preserving the flash memory tags.
- */
+/* If a string is returned, store the user feedback error, and return to the default view. */
 if(is_string($store)) {
     App::resolve('session')->flash('feedback', [
         'fail' => $store
@@ -21,72 +17,64 @@ if(is_string($store)) {
 /* Prep a final item variable, and check if we have an item stored. */
 $finalItem;
 
-if(is_array($store)) {
-    /* Make sure we store the correct autheurs ... W.I.P. ¿ */
-    if(!empty($store['autheurs']) && count($store['autheurs']) > 1) {
-        dd('Deal with multiple autheurs here ¿');
-    } else {
-        $finalItem['autheur'] = $store['autheurs'][0] ?? '';
-    }
-
-    /*  Check what type of isbn was returned,
-        always prefer storing the ISBN_13,
-        if there is non we try the ISBN_10,
-        incase that is also not there i just store a 0.
-     */
-    if(isset($store['isbn'][1]['type']) && $store['isbn'][1]['type'] === 'ISBN_13') {
-        $finalItem['isbn'] = $store['isbn'][1]['identifier'];
-    } elseif(isset($store['isbn'][0]['type']) && $store['isbn'][0]['type'] === 'ISBN_10') {
-        $finalItem['isbn'] = $store['isbn'][0]['identifier'];
-    } else {
-        $finalItem['isbn'] = 0;
-    }
-
-    /*  I also check if a tumbnail was returned,
-        if the large one isnt set,
-        i pick the small one,
-        and set nothing if non was set.
-     */
-    if(isset($store['cover']['thumbnail'])) {
-        $finalItem['cover'] = $store['cover']['thumbnail'];
-    } elseif(isset($store['cover']['smallThumbnail'])) {
-        $finalItem['cover'] = $store['cover']['smallThumbnail'];
-    } else {
-        $finalItem['cover'] = '';
-    }
-
-    /* Attempt to set the remaining data to store the item in the database. */
-    $finalItem['naam'] = $store['title'] ?? '';
-    $finalItem['datum'] = $store['date'] ?? '';
-    $finalItem['opmerking'] = $store['opmerking'] ?? '';
-    $finalItem['rIndex'] = $_POST['reeks-index'];
-}
-
-/* Check if the finalItem was stored properly, and prepare the correct flash memory data. */
-if(is_array($finalItem)) {
-    $flash = [
-        'oldItem' => $finalItem,
-        'newCover' => $finalItem['cover'],
-        'feedback' => [
-            'found' => 'Controleer de ingevulde gegevens van Google, het kan zijn dat deze niet helemaal klopt.'
-        ],
-        'tags' => [
-            'method' => 'PATCH',
-            'pop-in' => 'items-maken'
-        ]
-    ];
-/* Prepare a flesh error feedback is no item was set properly. */
+/* Make sure we store the correct autheurs ... W.I.P. ¿ */
+if(!empty($store['autheurs']) && count($store['autheurs']) > 1) {
+    dd('Deal with multiple autheurs here ¿');
 } else {
-    $flash = ['feedback' => [
-        'fail' => 'Er ging iets mis tijdens het ophalen van de item gegevens, neem contact op met uw Administrator!'
-    ]];
+    $finalItem['autheur'] = $store['autheurs'][0] ?? '';
 }
+
+/* Store the identifiers (isbn/ean codes), and try to get always get ISBN_13 first. */
+$isbn = $store['isbn'];
+
+if(is_array($isbn)) {
+    foreach($isbn as $okey => $pair) {
+        foreach($pair as $ikey => $value) {
+            if($ikey === 'type' && $value === 'ISBN_13') {
+                $finalItem['isbn'] = $isbn[$okey]['identifier'];
+            }
+
+            if($ikey === 'type' && $value === 'ISBN_10' && !isset($finalItem['isbn'])) {
+                $finalItem['isbn'] = $isbn[$okey]['identifier'];
+            }
+        }
+    }
+// Exception in case a single identifier is slightly different in code.
+} else {
+    dd('Deal with single identifiers here ¿');
+}
+
+/* Check if a cover was returned, and deal with all known solutions, exception is included incase i missed something */
+if(isset($store['cover'])) {
+    $cover = $store['cover'];
+
+    if(isset($cover['thumbnail'])) {
+        $finalItem['cover'] = $cover['thumbnail'];
+    } else if(isset($cover['smallThumbnail'])) {
+        $finalItem['cover'] = $cover['smallThumbnail'];
+    } else {
+        $finalItem['cover'] = 'Exception in cover code, review what went wrong !';
+    }
+}
+
+/* Attempt to set the remaining data to store the item in the database. */
+$finalItem['naam'] = $store['title'] ?? '';
+$finalItem['datum'] = $store['date'] ?? '';
+$finalItem['opmerking'] = $store['opmerking'] ?? '';
+$finalItem['rIndex'] = $_POST['reeks-index'];
+
+$flash = [
+    'oldItem' => $finalItem,
+    'newCover' => $finalItem['cover'],
+    'feedback' => [
+        'found' => 'Controleer de ingevulde gegevens van Google, het kan zijn dat deze niet helemaal klopt.'
+    ],
+    'tags' => [
+        'method' => 'PUT',
+        'pop-in' => 'items-maken'
+]];
 
 /* Always flash the outcome of the above evaluation, and redirect back to the items-maken pop-in or beheer page. */
 App::resolve('session')->flash($flash);
-
-if(!is_array($finalItem)) {
-    return App::redirect('beheer', TRUE);
-}
 
 return App::redirect('beheer#items-maken-pop-in', TRUE);
