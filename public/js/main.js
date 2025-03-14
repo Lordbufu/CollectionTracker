d1b6524185812fede82e07a6319297e14ea14a9e
LordBufu <bufu_39@hotmail.com>
2025-03-14 14:56:23 +0100

    Cleaning up and condensing the JS code, to prep for finalizing the project.

--- public/js/main.js
+++ public/js/main.js
@@ -4,56 +4,22 @@ let user, localDevice, html5QrcodeScanner, naamChecked = false, isbnChecked = fa
 document.onreadystatechange = () => {
     if(document.readyState === 'complete') {
         /* Store the device & user tag that was handed over from PhP. */
-        if(localStorage.device) {
-            localDevice = localStorage.device;
-            localStorage.removeItem('device');
-        }
-
-        if(localStorage.user) {
-            user = localStorage.user;
-            localStorage.removeItem('user');
-        }
-
+        if(localStorage.device) { localDevice = localStorage.device, localStorage.removeItem('device'); }
+        if(localStorage.user) { user = localStorage.user, localStorage.removeItem('user'); }
         /* In a few specific cases, i need to redirect to the default home page, and also clear the anchor tag. */
-        if(localStorage.reset) {
-            window.location = '/';
-            localStorage.removeItem('reset');
-        }
-
-        /* Deal with the scroll position triggers. */
-        if(sessionStorage.scrollPos) {
-            window.scrollTo(0, sessionStorage.scrollPos);
-
-            if(window.location.hash === '#reeks-maken-pop-in') {
-                dispatchInputEvent(localStorage.event);
-            } else if(window.location.hash === '#items-maken-pop-in') {
-                dispatchInputEvent(localStorage.event);
-            }
-
-            localStorage.removeItem('event');
-            sessionStorage.removeItem('scrollPos');
-        }
-
-        /* Elements and listen events for pop-in buttons */
+        if(localStorage.reset) { window.location = '/', localStorage.removeItem('reset'); }
+        /* Deal with the scroll position triggers, dispatching the correct events, based on the URL anchor tag (hash). */
+        if(sessionStorage.scrollPos) { window.scrollTo(0, sessionStorage.scrollPos); if(window.location.hash === '#reeks-maken-pop-in') { dispatchInputEvent(localStorage.event); } else if(window.location.hash === '#items-maken-pop-in') { dispatchInputEvent(localStorage.event); } localStorage.removeItem('event'), sessionStorage.removeItem('scrollPos'); }
+        /* Elements and listen events for pop-in submit buttons */
         const modalFormButt = document.getElementsByClassName('modal-form-button');
         const modalFormButtArr = Array.from(modalFormButt);
-
-        for(key in modalFormButtArr) {
-            modalFormButtArr[key].addEventListener('click', saveScroll);
-        }
-
+        for(key in modalFormButtArr) { modalFormButtArr[key].addEventListener('click', saveScroll); }
+        /* Elements and listen events for pop-in close buttons */
         const popInClButt = document.getElementsByClassName('modal-header-close');
         const clButtArr = Array.from(popInClButt);
-
-        for(key in clButtArr) {
-            clButtArr[key].addEventListener('click', saveScroll);
-        }
-
+        for(key in clButtArr) { clButtArr[key].addEventListener('click', saveScroll); }
         /* Initialize the static header & controller/menu bar for desktop devices. */
-        if(localDevice === 'desktop') {
-            initStatic();
-        }
-
+        if(localDevice === 'desktop') { initStatic(); }
     }
 }
 
@@ -62,17 +28,8 @@ document.onreadystatechange = () => {
             caller (string) - A string that allows me to see what called the event.
  */
 function dispatchInputEvent(caller) {
-    let inputEvent = new Event ('input', {
-        'bubbles': true,
-        'cancelable': false
-    });
-
-    if(caller == 'reeks-maken' && window.location.hash === '#reeks-maken-pop-in') {
-        return reeksMakenInput.dispatchEvent(inputEvent);
-    } else if(caller == 'item-maken' && window.location.hash === '#items-maken-pop-in') {
-        itemNaamInp.dispatchEvent(inputEvent);
-        return itemIsbnInp.dispatchEvent(inputEvent);
-    }
+    let inputEvent = new Event ('input',{'bubbles': true,'cancelable': false});
+    if(caller == 'reeks-maken' && window.location.hash === '#reeks-maken-pop-in') { return reeksMakenInput.dispatchEvent(inputEvent); } else if(caller == 'item-maken' && window.location.hash === '#items-maken-pop-in') { return itemNaamInp.dispatchEvent(inputEvent), itemIsbnInp.dispatchEvent(inputEvent); }
 }
 
 /*  saveScroll(e):
@@ -81,12 +38,8 @@ function dispatchInputEvent(caller) {
             e (object)  - The event that was assigned to this listen event, used to see what element triggered it.
  */
 function saveScroll(e) {
-    caller = e.target.id;
-    sessionStorage.setItem('scrollPos', window.scrollY);
-
-    if(e.target.className === 'item-bew-butt button') {
-        caller = e.target.className;
-    }
+    caller = e.target.id, sessionStorage.setItem('scrollPos', window.scrollY);
+    if(e.target.className === 'item-bew-butt button') { caller = e.target.className; }
 
     switch(caller) {
         case 'reeks-pop-req':
@@ -105,113 +58,43 @@ function saveScroll(e) {
     }
 }
 
-/*  itemVerwijderen(e):
-    A simple confirmation check, that displays the reeks name, and triggers the submit button base on said confirmation.
-        rowCol  - The table row in witch the button was pressed.
-        rowArr  - The table row in array format for easier access.
-        conf    - The confirmation box when the button is pressed.
-
-    Return Value: Boolean.
-*/
-function itemVerwijderen(e) {
-    const rowCol = document.getElementsByClassName('item-tafel-inhoud');
-    const rowArr = Array.from(rowCol);
-    const conf = confirm('Weet u zeker dat het Item: ' + rowArr[0].children[2].innerHTML + '\n Verwijderen moet worden ?');
-
-    if(conf) {
-        return true;
-    } else {
-        if(sessionStorage.scrollPos)  {
-            sessionStorage.removeItem('scrollPos');
-        }
-        return false;
-    }
-}
-
-/*  naamCheck(e):
-        This function listens to input changes in serie/item name fields, and evaluates if valid and enables/disabled the submit button.
-        It also change the input style, based on the evaluation, and works in tandem with isbnCheck(e) if there is a isbn input field.
-            e       - The listen event object.
-            uInp    - The user input, set from the listen event object.
-            elStyle - The element style of the input element, set from the listen event object.
-
-        External Variables (defined globally, instanced in the init):
-            createSerieSubm / editSerieSubm - Create/Edit subm buttons for series.
-            createAlbumSubm / editAlbumSubm - Create/Edit subm buttons for albums.
-        
-        Return Value: None.
- */
+/* naamCheck(e): Simple validate for the name inputs, with some visual feedback, working in tandem with isbnCheck for items. */
 function naamCheck(e) {
-    const uInp = e.target.value;
-    const elStyle = e.target.style;
-
+    const uInp = e.target.value, elStyle = e.target.style;
+    /* Check if there is an input to check. */
     if(uInp !== '' && uInp !== null && uInp !== undefined) {
-        elStyle.outline = '3px solid green';
-        naamChecked = true;
-
-        if(e.target.id === 'reeks-maken-naam') {
-            return reeksMakenSubmit.disabled = false;
+        /* Only trigger the main path if the input is a similar lenght as PhP is expecting. */
+        if(uInp.length >= 7 && uInp.length <= 35) {
+            elStyle.outline = '3px solid green', naamChecked = true;
+            /* Enable the pop-in submit button, for items only if the isbn checked tag is also true. */
+            if(e.target.id === 'reeks-maken-naam') { return reeksMakenSubmit.disabled = false; }
+            if(e.target.id === 'item-maken-naam') { if(isbnChecked) { return itemMakenSubm.disabled = false; } }
+            return; // required to skip the failed conditions, that used to be in else statements.
         }
-
-        if(e.target.id === 'item-maken-naam') {
-            if(isbnChecked) {
-                return itemMakenSubm.disabled = false;
-            }
     }
-    } else {
-        naamChecked = false;
-
-        if(e.target.id === 'reeks-maken-naam') {
-            reeksMakenInput.disabled = true;
-        }
-
-        if(e.target.id === 'item-maken-naam') {
-            itemMakenSubm.disabled = true;
-        }
-        
-        return elStyle.outline = '3px solid red';
-    }
+    /* Fail condition path, disable pop-in submit buttons depending on target id, set tag to false, and give red outline. */
+    naamChecked = false;
+    if(e.target.id === 'reeks-maken-naam') { reeksMakenInput.disabled = true; }
+    if(e.target.id === 'item-maken-naam') { itemMakenSubm.disabled = true; }
+    return elStyle.outline = '3px solid red';
 }
 
-/*  isbnCheck(e):
-        This function listens to input changes in album isbn fields, and evaluates if valid and enables/disabled the submit button.
-        It also change the input style, based on the evaluation, and works in tandem with naamCheck(e).
-            e       - The listen event object.
-            uInp    - The user input, set from the listen event object.
-            elStyle - The element style of the input element, set from the listen event object.
-
-        External Variables (defined globally, instanced in the init):
-            createSerieSubm / editSerieSubm - Create/Edit subm buttons for series.
-            createAlbumSubm / editAlbumSubm - Create/Edit subm buttons for albums.
-
-        Return Value: None.
-    */
+/* isbnCheck(e): Validate the isbn input, and allow submition if the value is 0 or the length is 10 or 13 numbers, work in tandem with naamCheck(e). */
 function isbnCheck(e) {
-    const uInp = e.target.value
-    const elStyle = e.target.style;
-
+    const uInp = e.target.value, elStyle = e.target.style;
+    /* Check if there is an input to check. */
     if(uInp !== '' && uInp !== null && uInp !== undefined) {
-        const isbn = uInp.replace(/-/g, '');
-        const filter = /[a-zA-z]/g;
-        const letters = filter.test(isbn);
-
+        /* Replace any any '-' with nothing, create a letter filter, and check if there are no letters. */
+        const isbn = uInp.replace(/-/g, ''), filter = /[a-zA-z]/g, letters = filter.test(isbn);
+        /* If there are no letters, and the isbn is either '0' or has the length of 10 or 13 numbers. */
         if(!letters && isbn === '0' || isbn.length === 10 || isbn.length === 13) {
-            elStyle.outline = '3px solid green';
-            e.target.value = isbn;
-            isbnChecked = true;
-            if(naamChecked) {
-                return itemMakenSubm.disabled = false;
-            }
-            return;
-        } else {
-            isbnChecked = false;
-            itemMakenSubm.disabled = true;
-            return elStyle.outline = '3px solid red';
+            /* Set the checked tag, and change the style and fill in the filtered isbn. */
+            elStyle.outline = '3px solid green', e.target.value = isbn, isbnChecked = true;
+            /* Only if the name is also validated, the submit button is enabled. */
+            if(naamChecked) { return itemMakenSubm.disabled = false; }
+            return; // required to skip the failed conditions, that used to be in else statements.
         }
-    } else {
-        isbnChecked = false,
-        itemMakenSubm.disabled = true;
-        return elStyle.outline = '3px solid red';
     }
-    
+    /* In all other cases the tag is false, the outline is red, and the submit button is disabled */
+    return isbnChecked = false, itemMakenSubm.disabled = true, elStyle.outline = '3px solid red';
 }
