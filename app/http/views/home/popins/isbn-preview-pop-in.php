491e67f637e45ebaaab507d7dca823232a1d271d
LordBufu <bufu_39@hotmail.com>
2025-03-21 14:05:23 +0100

    Cleaning\double checking some isbn title choices code.

--- app/http/views/home/popins/isbn-preview-pop-in.php
+++ app/http/views/home/popins/isbn-preview-pop-in.php
@@ -1,25 +1,32 @@
-<?php if(isset($_SESSION['_flash']['isbn-choices'])) { $store = $_SESSION['_flash']['isbn-choices']; unset($_SESSION['_flash']['tags']); } ?>
+<?php if(isset($_SESSION['_flash']['isbn-choices'])) { $store = $_SESSION['_flash']['isbn-choices']; } ?>
 <div id="isbn-preview" class="modal-cont">
     <div class="modal-content-cont">
+
         <div class="modal-header-cont">
             <h3 class="modal-header-text">ISBN Search Review</h3>
+
             <?php if(isset($_SESSION['user']['rights']) && $_SESSION['user']['rights'] === 'user') : ?>
             <form class="modal-header-close-form" method="post" action="/gebruik">
                 <input class="modal-header-input" name="close" value="back" hidden/>
                 <input class="modal-header-close" type="submit" value="&times;"/>
             </form>
+
             <?php elseif(isset($_SESSION['user']['rights']) && $_SESSION['user']['rights'] === 'admin') : ?>
             <form class="modal-header-close-form" method="post" action="/beheer">
                 <input class="modal-header-input" name="close" value="back" hidden/>
                 <input class="modal-header-close" type="submit" value="&times;"/>
             </form>
+
             <?php endif; ?>
         </div>
+
         <div class="modal-body">
-            <form class="modal-form" enctype="multipart/form-data" method="post" action="/scanConf">
-                <input class="modal-form-isbn" name="isbn-choice" value="<?=$store['isbn']?>" hidden/>
-                <input class="modal-form-serie-index" name="reeks-index" value="<?=$store['index']?>" hidden/>
-                <div class="modal-form-left-cont" id="modal-form-left-cont">
+            <div class="modal-form-left-cont" id="modal-form-left-cont">
+
+                <form class="modal-form" enctype="multipart/form-data" method="post" action="/scanConf">
+                    <input class="modal-form-isbn" name="isbn-choice" value="<?=$store['isbn']?>" hidden/>
+                    <input class="modal-form-serie-index" name="reeks-index" value="<?=$store['index']?>" hidden/>
+
                     <select class="modal-form-select" name="title-choice" id="item-choice" required>
                         <option class="modal-form-title-options" value="">Selecteer een title</option>
                         <?php   // Ignore the 'Titles', 'isbn' & 'index' entries.
@@ -29,13 +36,16 @@
                         <?php   endif;
                             endforeach; ?>
                     </select>
+
                     <div class="butt-box">
                         <input class="modal-form-button button" id="prevSubm" type="submit" value="Bevestigen"/>
                     </div>
-                </div>
-                <div class="modal-form-right-cont"></div>
-            </form>
+                </form>
+
+            </div>
         </div>
+
     </div>
 </div>
+
 <script> const prevSubm = document.getElementById('prevSubm'); prevSubm.addEventListener('click', saveScroll); </script>
