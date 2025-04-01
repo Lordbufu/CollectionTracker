d0d3d7e740d9299f7a98520be8cd6d4a259dc771
LordBufu <bufu_39@hotmail.com>
2025-04-01 13:08:42 +0200

    Adding extra routes for various users, resolving a issue with new browser tabs not loading styles because it was the wrong route.

--- app/http/controllers/index.php
+++ app/http/controllers/index.php
@@ -3,7 +3,21 @@
 use App\Core\App;
 
 /* Return the 'trap' view, if no user was set. */
-if(!isset($_SESSION['user']['rights'])) { return App::view('index.view.php'); }
+if(!isset($_SESSION['user']['rights'])) {
+    return App::view('index.view.php');
+}
 
-/* Happy path redirecting to the actual homepage. */
-return App::redirect('home', TRUE);
+/* Redirect guests to the guest page, preserving any session _flash memory. */
+if(isset($_SESSION['user']['rights']) && $_SESSION['user']['rights'] === 'guest') {
+    return App::redirect('home', TRUE);
+}
+
+/* Redirect users to the gebruik page, preserving any session _flash memory. */
+if(isset($_SESSION['user']['rights']) && $_SESSION['user']['rights'] === 'user') {
+    return App::redirect('gebruik', TRUE);
+}
+
+/* Redirect admin to the beheer page, preserving any session _flash memory. */
+if(isset($_SESSION['user']['rights']) && $_SESSION['user']['rights'] === 'admin') {
+    return App::redirect('beheer', TRUE);
+}
